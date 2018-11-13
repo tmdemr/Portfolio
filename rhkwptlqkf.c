ec961eaebd8c4c4dff70042fa40494978af305a5
yongjjang <pastelhawk@gmail.com>
2018-11-13 23:58:17 +0900

    Add files via upload

--- rhkwptlqkf.c
+++ rhkwptlqkf.c
@@ -8,14 +8,22 @@ struct Record{
 struct Record mas;
 struct Record tra;
 
-int main() {
+void get_transaction_record();
+void get_master_record();
+
 	FILE *old_master, *transaction, *new_master, *error;
+
+
 	if ((old_master = fopen("master.txt", "r")) == NULL){
 		printf("file open error\n");
+		exit(-1);
 	}
+
 	if ((transaction = fopen("transaction.txt", "r")) == NULL){
 		printf("file open error\n");
+		exit(-1);
 	}
+
 	new_master = fopen("new_master.txt", "w");
 	error = fopen("error.txt", "w");
 
@@ -27,72 +35,50 @@ int main() {
 	m_file_pointer = fscanf(old_master,"%d %d", &mas.no, &mas.ammount);
 	t_file_pointer = fscanf(transaction,"%d %d", &tra.no, &tra.ammount);
 
-
-
-  void isMasEOF(){
+  void get_master_record(){
     if (m_file_pointer == EOF){
-        mas.no = EOF;
-      }else {
+		        mas.no = EOF;}
+      else {
         printf("%d %d\n",mas.no,mas.ammount);
         m_file_pointer = fscanf(old_master,"%d %d", &mas.no, &mas.ammount);
       }
     }
 
-  void isTraEOF(){
+  void get_transaction_record(){
     if (t_file_pointer == EOF){
-        tra.no = EOF;
-      }else
+        tra.no = EOF;}
+      else
         t_file_pointer = fscanf(transaction,"%d %d", &tra.no, &tra.ammount);
-
     }
 
-	while(mas.no != EOF && tra.no != EOF){
-    if(mas.no <  tra.no){
-			fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
-      isMasEOF();
-    }
-		if(mas.no <= tra.no){
-			if(mas.no == tra.no){
+int main(int argc, char const *argv[]) {
+
+		while(mas.no != EOF && tra.no != EOF){
+	    if(mas.no < tra.no){
+	      fprintf(new_master, "%d %d\n", mas.no, mas.ammount);
+	      get_master_record();
+	    } else if (mas.no == tra.no) {
 	      if ((mas.ammount + tra.ammount) < 0){
 	        error_type = 'L';
 	        fprintf(error,"%c %d %d\n",error_type, tra.no, tra.ammount);
-					fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
-			    isMasEOF();
-			    isTraEOF();
+	        fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
+	        get_master_record();
+	        get_transaction_record();
 	      }else{
 	        mas.ammount = mas.ammount + tra.ammount;
-					fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
-	        isMasEOF();
-	        isTraEOF();
+	        fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
+	        get_master_record();
+	        get_transaction_record();
 	      }
-			}
-		}else{
-			error_type = 'U';
-			fprintf(error, "%c %d %d\n",error_type, tra.no, tra.ammount );
-			isTraEOF();
-			fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
-			isMasEOF();
-		}
-/*
-void except(){
-	switch (error_type) {
-		case 'L':
-			fprintf(error,"%c %d %d\n",error_type, tra.no, tra.ammount);
-			fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
-	    isMasEOF();
-	    isTraEOF();
-			break;
-		case 'U':
-			fprintf(error, "%c %d %d\n",error_type, tra.no, tra.ammount );
-			isTraEOF();
-			fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
-			isMasEOF();
-			break;
-	}
-}
-*/
-  }
+	    } else {
+	      error_type = 'U';
+	      fprintf(error, "%c %d %d\n",error_type, tra.no, tra.ammount );
+	      get_transaction_record();
+	      fprintf(new_master,"%d %d\n",mas.no, mas.ammount);
+	      get_master_record();
 
+	    }
+		}
 
 	fclose(old_master);
 	fclose(transaction);
